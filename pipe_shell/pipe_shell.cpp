2813a253bbdc2480f51c505a610b6edf8e53d325
Nicoledon <3121506708@qq.com>
2025-07-07 19:32:20 +0800

    add pipe_shell

---
+++ pipe_shell/pipe_shell.cpp
@@ -0,0 +1,11 @@
+#include <unistd.h>    // for fork()
+#include <sys/types.h> // for pid_t
+#include <sys/wait.h>  // for wait(), waitpid(), etc.
+
+#include <cstdlib>  // for exit(), EXIT_SUCCESS, and EXIT_FAILURE
+
+int main() {
+  // Todo: implement
+  return EXIT_SUCCESS;
+}
+
