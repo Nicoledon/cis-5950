ac768fdd8d715f01e4e7fa527e146f8805901b11
Nicoledon <3121506708@qq.com>
2025-07-08 06:39:54 +0000

    add basic fuction for pipe_shell

--- pipe_shell/pipe_shell.cpp
+++ pipe_shell/pipe_shell.cpp
@@ -1,11 +1,60 @@
-#include <unistd.h>    // for fork()
-#include <sys/types.h> // for pid_t
-#include <sys/wait.h>  // for wait(), waitpid(), etc.
-
-#include <cstdlib>  // for exit(), EXIT_SUCCESS, and EXIT_FAILURE
-
-int main() {
-  // Todo: implement
-  return EXIT_SUCCESS;
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <vector>
+using namespace std;
+vector<string> sqlit(const string &line) {
+  vector<string> words;
+  istringstream iss(line);
+  string word;
+  while (getline(iss, word, ' ')) {
+    words.push_back(word);
+  }
+  return words;
+}
+char **vec2arg(const vector<string> &words) {
+  char **arg = new char *[words.size() + 1];
+  for (auto i = 0; i < words.size(); i++) {
+    arg[i] = const_cast<char *>(words[i].c_str());
+  }
+  arg[words.size()] = nullptr;
+  return arg;
+}
+int main(int argc, char **argv) {
+  string line;
+  while (true) {
+    cout << "$ ";
+    if (!getline(cin, line)) {
+      if (!cin.eof()) {
+        cerr << "Warning: fatal error while reading input from user" << endl;
+      }
+      cout << endl;
+      break;
+    } else if (line == "exit") {
+      cout << endl;
+      break;
+    } else {
+      auto words = sqlit(line);
+      auto arg = vec2arg(words);
+      pid_t pid = fork();
+      if (pid == 0) {
+        execvp(arg[0], arg);
+        exit(EXIT_FAILURE);
+      } else {
+        int status;
+        waitpid(pid, &status, 0);
+      }
+      delete[] arg;
+    }
+  }
+  return 0;
 }
 
+// Nothing here!
+// Highly recommend you look at stdin_echo.cpp for getting started.
+// The lecure example code (and the specification for this assignment) are also
+// good to read.
