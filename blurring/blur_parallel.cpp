459f53f8cd0b4347c44a894f2010da2da4766c30
Nicoledon <3121506708@qq.com>
2025-07-14 13:10:15 +0000

    finnish but didnot finnish perf

--- blurring/blur_parallel.cpp
+++ blurring/blur_parallel.cpp
@@ -1,38 +1,46 @@
 #include "qdbmp.hpp"
+#include <array>
 #include <cstdlib>
 #include <iostream>
 #include <pthread.h>
 #include <string>
 #include <vector>
-#include <array>
+#include <stack>
+using std::array;
 using std::cerr;
 using std::endl;
 using std::string;
-using std::array;
-constexpr UCHAR MAX_COLOR_VALUE = 255U;
+using std::vector;
+using std:: stack;
 pthread_mutex_t lock;
-struct thread_item {
-        int height;
-        int width;
-        int y;
-        int x;
-        int size;
-        BitMap *image;
-        BitMap *average;      
+struct thd_arg {
+  int height;
+  int width;
+  int y;
+  int x;
+  int size;
+  BitMap *ref;
+  BitMap *image;
 };
-void *average_pixel(void * arg) {
-  struct thread_item *item = reinterpret_cast<struct thread_item *> (arg);
+void *average_pixel(void *arg) {
+
   UINT sum_r = 0;
   UINT sum_g = 0;
   UINT sum_b = 0;
   UINT count = 0;
-  std::array<UINT, 3> vec;
-  for (int i = item->x - item->size; i <= item->x + item->size; i++) {
-    for (int j = item->y - item->size; j <= item->y + item->size; j++) {
-      if (i < 0 || j < 0 || i >= item->width || j >= item->height) {
+  struct thd_arg *a = reinterpret_cast<struct thd_arg *>(arg);
+  int height = a->height;
+  int width = a->width;
+  int y = a->y;
+  int x = a->x;
+  int size = a->size;
+  BitMap *image = a->image;
+  for (int i = x - size; i <= x + size; i++) {
+    for (int j = y - size; j <= y + size; j++) {
+      if (i < 0 || j < 0 || i >= width || j >= height) {
         continue;
       } else {
-        RGB color = item->image->get_pixel(i, j);
+        RGB color = a->ref->get_pixel(i, j);
         sum_r += color.red;
         sum_g += color.green;
         sum_b += color.blue;
@@ -40,17 +48,16 @@ void *average_pixel(void * arg) {
       }
     }
   }
-  vec[0] = (sum_r / count);
-  vec[1] = (sum_g / count);
-  vec[2] = (sum_b / count);
-  RGB average_color{static_cast<UCHAR>(vec[0]), static_cast<UCHAR>(vec[1]),
-                    static_cast<UCHAR>(vec[2])};
-
-  // Set the negative color
+  sum_r /= count;
+  sum_g /= count;
+  sum_b /= count;
+  RGB average_color{static_cast<UCHAR>(sum_r), static_cast<UCHAR>(sum_g),
+                    static_cast<UCHAR>(sum_b)};
+  
   pthread_mutex_lock(&lock);
-  item->average->set_pixel(item->x, item->y, average_color);
+  image->set_pixel(x, y, average_color);
   pthread_mutex_unlock(&lock);
-  delete item;
+  delete a;
   return nullptr;
 }
 /**
@@ -59,15 +66,17 @@ void *average_pixel(void * arg) {
  */
 int main(int argc, char *argv[]) {
   // Check input commands
-  if (argc != 4) {
+  if (argc != 5) {
     cerr << "Usage: " << argv[0] << " <input file> <output file>" << endl;
     return EXIT_FAILURE;
   }
-  pthread_mutex_init(&lock , nullptr);
+
   string input_fname{argv[1]};
   string output_fname{argv[2]};
   string str_size{argv[3]};
+  string thread_size{argv[4]};
   int size = stoi(str_size);
+  int n_threads = stoi(thread_size);
   // Construct a BitMap object using the input file specified
   BitMap image(input_fname);
 
@@ -87,26 +96,40 @@ int main(int argc, char *argv[]) {
     perror("ERROR: Failed to open BMP file.");
     return EXIT_FAILURE;
   }
-
+  pthread_t thds[n_threads];
+  pthread_mutex_init(&lock, nullptr);
+  int count = 0;
+  stack<struct thd_arg *> vec;
   // Loop through each pixel and turn into negative
-  const int n_thread = height * width;
-  array<pthread_t , n_thread>thds{};
   for (size_t y = 0; y < height; ++y) {
     for (size_t x = 0; x < width; ++x) {
       // Read the current pixel RGB color
-      struct thread_item * args = new struct thread_item;
-      args->height = (int)height;
-      args->width = (int)width;
-      args->y = y ;
-      args->x = x;
-      args->image = &image;
-      args->average = &average;
-      pthread_create(&thds.at(y * x) , nullptr , &average_pixel , args);
       // Calculate the negative RGB color
+      struct thd_arg *args = new struct thd_arg;
+      args->width = width;
+      args->height = height;
+      args->x = x;
+      args->y = y;
+      args->size = size;
+      args->ref = &image;
+      args->image = &average;
+      vec.push(args);
+      // Set the negative color
     }
   }
-  for(int i = 0 ; i < n_thread ; i ++){
-      pthread_join(thds.at(i) ,nullptr);
+  while(!vec.empty()){
+      auto arg = vec.top();
+      vec.pop();
+      pthread_create(&thds[count++] , nullptr , &average_pixel , arg);
+      if(count == n_threads){
+          for (int i = 0 ; i < count ; i ++){
+              pthread_join(thds[i] , nullptr);
+          }
+          count = 0;
+      }
+  }
+  for(int i = 0 ;i < count ; i ++ ){
+      pthread_join(thds[i] , nullptr);
   }
   pthread_mutex_destroy(&lock);
   // Output the negative image to disk
